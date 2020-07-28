d9e4fa01672e6d27c038d92b805ad2c178e52ec4
Guilherme <guilherme121225@gmail.com>
2020-07-27 22:10:50 -0300

    Atualização

--- Index.js
+++ Index.js
@@ -1,28 +1,76 @@
 // Criar um programa que calcula a média
-// das notas entre os alunos e envia
+// das turmas e  envia
 // mensagem do cálculo da média.
 
+const alunosDaTurmaA = [
+    {
+        nome: "Mayk",
+        nota: 10
+    },
 
-const aluno01 = 'Gui'
-const notaAluno01 = 9.8
+    {
+        nome: "Diego",
+        nota: 10
+    },
 
-const aluno02 = 'Diego'
-const notaAluno02 = 10
+    { 
+        nome: "Fulano",
+        nota: 8
+    },
+    { 
+        nome: "Mais um aluno",
+        nota: 1
+    }
 
-const aluno03 = 'Fulano'
-const notaAluno03 = 2 
-
-const media = (notaAluno01 + notaAluno02 + notaAluno03) / 3
+]
 
 
-// Se média for maior que 5, parabenizar a turma
-if(media > 5){
+const alunosDaTurmaB = [
+    {
+        nome: "Cleiton",
+        nota: 1.8
+    },
 
-    console.log(`A média foi de  ${media}. Parabéns`)
+    {
+        nome: "Robson",
+        nota: 10
+    },
 
-}else{
-    console.log(`A média é menor que 5`)
+    { 
+        nome: "Siclano",
+        nota: 0
+    },
+    { 
+        nome: "Novo Aluno",
+        nota: 0
+    }
+]
+
+
+function calculaMedia(alunos){
+    let soma = 0;
+    for(let i = 0; i < alunos.length; i++){
+        soma = soma + alunos[i].nota
+    }
+
+    const media = soma / alunos.length
+    return media
+}
+
+function enviaMensagem(media, turma){
+    //Se a média for maior que 5, parabenizar a turma
+    if (media > 5) {
+       console.log(`A média da ${turma} foi de ${media}. Parabéns`)
+    } else {
+       console.log(`A média da turma ${turma} foi menor que 5`)
+    }
+    
 }
 
 
-console.log(media > 5)
+const media1 = calculaMedia(alunosDaTurmaA)
+const media2 = calculaMedia(alunosDaTurmaB)
+
+enviaMensagem(media1, 'turmaA')
+enviaMensagem(media2, 'turmaB')
+
